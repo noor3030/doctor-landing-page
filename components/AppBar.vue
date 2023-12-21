763fc7100db491417d339e3111e8c341c41e9895
NOOR HAIDER <72420114+noor3030@users.noreply.github.com>
2023-12-20 19:18:52 -0800

    Update AppBar and Navbar styles

--- components/AppBar.vue
+++ components/AppBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="grid grid-cols-2 pt-16 px-16 ">
+  <div class="grid grid-cols-2 mt-16 px-16" id="home">
     <div class="text-section justify-start content-start flex flex-col">
       <div class="text-5xl flex row space-x-5 font-bold">
         <h1 class="ml-4">ابحث عن</h1>
@@ -10,11 +10,13 @@
 
       <p class="mt-10 text-2xl text-[#646566]">
         مع اكبر تطبيق لحجز المواعيد الطبية في العراق اضمن صحتك وصحة عائلتك مع
-        تطبيق صحتي
+        تطبيق
       </p>
+      <h1 class="text-4xl font-bold text-[#00c3d3] mt-2">CARE</h1>
+
       <div class="flex actions">
         <button
-          class="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 rounded-lg text-2xl px-5 py-2 mt-10"
+          class="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 rounded-lg text-3xl px-5 py-2 mt-10"
         >
           حمل التطبيق الان
         </button>
@@ -24,7 +26,7 @@
       <img
         src="../public/doctors-images/appbar-image.png"
         alt=""
-       class="object-contain w-5/6	 h-5/6"
+        class="object-contain w-5/6 h-5/6"
       />
     </div>
   </div>
