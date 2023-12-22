fff2805d7e9b90d906d2ee47c17c9131aea0b3e0
NOOR HAIDER <72420114+noor3030@users.noreply.github.com>
2023-12-21 23:31:35 -0800

    Refactor Footer component layout

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bg-[#081839] bottom-0 mt-16 text-white md:p-20 p-6">
     <img src="../public/logos/slogan.svg" class="w-36 h-36" />
-    <div class="md:grid md:grid-cols-4 md:gap-x-12 flex flex-col gap-y-10">
+    <div class=" flex flex-col gap-y-10">
       <div class="flex flex-col">
         <h1 class="text-xl">
           استمتع بتجربة طبية فريدة مع تطبيقنا الحصري الاول من نوعه وميزاته
@@ -116,10 +116,10 @@
 
       <div class="flex flex-col gap-y-6">
         <h1 class="text-2xl font-bold">لتحميل التطبيق</h1>
-        <a href="" class="w-1/2">
+        <a href="" class="w-1/2 md:w-1/5">
           <img src="../public/app-store.svg" alt=""
         /></a>
-        <a href="" class="w-1/2">
+        <a href="" class="w-1/2 md:w-1/5" >
           <img src="../public/google-play.svg" alt="" />
         </a>
       </div>
