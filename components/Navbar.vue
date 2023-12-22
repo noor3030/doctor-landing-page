388072d5ef3c1bf077af439afe3b923cb0ec9997
NOOR HAIDER <72420114+noor3030@users.noreply.github.com>
2023-12-21 16:36:23 -0800

    Refactor Navbar component styling

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -23,51 +23,46 @@
   <div class="md:hidden">
     <div
       :class="open ? ' opacity-25' : 'opacity-100'"
-      class="grid grid-cols-2 w-full py-4 px-5"
+      class="flex w-full py-4 px-5 bg-[#cefbfe] justify-between"
     >
-      <button @click="tog()" class="text-black text-2xl font-bold w-fit">
+      <img src="../public/logos/slogan.svg" alt="" class="w-1/4" />
+
+      <button @click="tog()" class="font-bold text-xl text-[#0d1c3e]">
         &#9776;
       </button>
-      <img src="../public/logos/slogan.svg" alt="" class="w-3/4 self-center" />
     </div>
     <nav
       :class="open ? 'navbar-open' : 'navbar-close'"
-      class="navbar w-64 absolute overflow-x-scroll bg-gray-50 top-0 h-screen"
+      class="navbar w-full absolute overflow-x-scroll bg-[#cefbfe] top-0 h-screen"
     >
-      <div class="flex pr-2 justify-end">
-        <button @click="tog()" class="p-2 text-black text-xl font-bold">
+      <div class="flex p-2">
+        <button @click="tog()" class="p-2  text-xl font-bold">
           &#9747;
         </button>
       </div>
 
-      <ul class="list-none text-black text-center flex flex-col gap-y-3">
+      <ul class="list-none  text-center flex flex-col gap-y-3">
         <li><h1 class="text-xl text-center font-bold">القائمة</h1></li>
         <li>
-          <a href="#home" class="hover:bg-white rounded-lg p-2"> الرئيسية</a>
+          <a href="#home"> الرئيسية</a>
         </li>
         <li>
-          <a href="#treatment" class="hover:bg-white rounded-lg p-2"
-            >العلاجات</a
-          >
+          <a href="#treatment">العلاجات</a>
         </li>
         <li>
-          <a href="#about-us" class="hover:bg-white rounded-lg p-2">من نحن </a>
+          <a href="#about-us">من نحن </a>
         </li>
         <li>
-          <a href="#service" class="hover:bg-white rounded-lg p-2">الخدمات</a>
+          <a href="#service">الخدمات</a>
         </li>
         <li>
-          <a href="#doctors" class="hover:bg-white rounded-lg p-2">الاطباء</a>
+          <a href="#doctors">الاطباء</a>
         </li>
         <li>
-          <a href="#articles" class="hover:bg-white rounded-lg p-2"
-            >المدونات
-          </a>
+          <a href="#articles">المدونات </a>
         </li>
         <li>
-          <a href="contact-us" class="hover:bg-white rounded-lg p-2"
-            >تواصل معنا</a
-          >
+          <a href="contact-us">تواصل معنا</a>
         </li>
       </ul>
     </nav>
